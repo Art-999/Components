b381765972b6463bc502db0bfe616b14453d1b17
artur.musayelyan <musayelyan099@gmail.com>
2017-11-23 17:24:46 +0400

    23.11.2017 ArtCommit gorcic ( 5 )

--- app/src/main/java/com/example/arturmusayelyan/components/FarmePagerAdapter.java
+++ app/src/main/java/com/example/arturmusayelyan/components/FarmePagerAdapter.java
@@ -8,10 +8,10 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by artur.musayelyan on 23/11/2017.
  */
 
-public class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+public class FarmePagerAdapter extends FragmentPagerAdapter {
 
     int colorArray[]={android.R.color.holo_red_light,android.R.color.holo_blue_bright,android.R.color.holo_green_light,android.R.color.holo_purple,android.R.color.holo_orange_light};
-    public FragmentPagerAdapter(FragmentManager fm) {
+    public FarmePagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
